d6f9eac089a01491ff474035d2b9f4b74eb79669
Ngumo-1 <morrisngumo7@gmail.com>
2024-01-25 14:42:02 +0300

    finding determinant of a matrix/vector

---
+++ normalize.py
@@ -0,0 +1,11 @@
+import numpy as np
+from numpy import random
+
+# arr=np.random.randint(100, size=(3,3))
+# print(arr)
+
+def determinant(arr):
+    return np.linalg.det(arr)
+
+# xarr=determinant(arr)
+# print(xarr)
